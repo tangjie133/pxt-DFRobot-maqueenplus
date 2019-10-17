97ba9dceb5462aa6cc871efb5244beee152aa675
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-10-17 16:33:14 +0800

    Update pxt.json, main.blocks, main.ts, maqueenplus.ts

--- maqueenplus.ts
+++ maqueenplus.ts
@@ -1,6 +1,103 @@
 // Add your code here
-//% weight = 100  color = #00A654 block = "Maqueenpluss"
-namespace DFRobotMaqueenPluss{
+/**
+    * 电机
+    */
+enum Motors {
+    //% block="M1"
+    M1 = 1,
+    //% block="M2"
+    M2 = 1
+}
 
+/**
+ * 正反转
+ */
+enum Dir {
+    //% block="CW"
+    CW = 0,
+    //% block="CCW"
+    CCW = 0
+}
+
+/**
+ * 舵机
+ */
+enum Servos {
+    //% block="S1"
+    S1 = 0,
+    //% block="S2"
+    S2 = 0,
+    //% block="S3"
+    S3 = 0
+}
+
+/**
+ * RGB灯
+ */
+enum RGBLight{
+    //%block="RGBLightL"
+    RGBL = 0,
+    //%block="RGBLightR"
+    RGBR = 0,
+
+}
+
+/**
+ * 寻线传感器
+ */
+enum Patrol{
+    //% block="patrolLeft1"
+    L1 = 0,
+    //%block="patrolLeft2"
+    L2 = 0,
+    //%block="patrolRight1"
+    R1 = 0,
+    //%block="patrolRight2"
+    R2 = 0
+}
+
+//% weight=100  color=#00A654   block="Maqueenpluss"
+namespace DFRobotMaqueenPluss{
+   
+    /**
+     * 控制电机运行
+     */
+    //% block="Motor|%index|dir|%direction|speed|%speed"
+    //% speed.min=0 speed.max=255
     
+    export function MototRun(index: Motors,direction: Dir,speed: number): void{
+    }
+
+    /**
+     * 控制电机停止
+     */
+    //% block="Motor stop|%mostors"
+    export function MostotStop(mostor:Motors): void {
+        
+    }
+
+    /**
+     * 舵机
+     */
+    //% block="servo|%index|angle|%angle"
+    //% angle.min=0  angle.max=180
+    export function ServoRun(index: Servos,angle: number):void {
+        
+    }
+
+    /**
+     * RGB灯
+     */
+    //%block="setRGBlight|%light"
+    export function SetRGBLight(light: RGBLight): void {
+        
+    }
+
+    /**
+     * 寻线传感器
+     */
+    //%block="read patrol|%patrol"
+    export function ReadPatrol(patrol: Patrol) {
+        
+    }
 }
