f4f461eb5dc40d3e3c2288d0c56151134333b5f9
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-10-23 15:40:44 +0800

    Update main.blocks, main.ts, maqueenplus.ts

--- maqueenplus.ts
+++ maqueenplus.ts
@@ -1,4 +1,3 @@
-
 /**
     * 电机
     */
@@ -69,7 +68,7 @@ enum Patrol {
 /**
  * 超声波单位
  */
-enum Sonicunit{
+enum Sonicunit {
     //% block="cm"
     Centimeters,
     //% block="μs"
@@ -79,7 +78,7 @@ enum Sonicunit{
 /**
  * PID开关
  */
-enum PID{
+enum PID {
     //%block="OFF"
     OFF = 0,
     //%block="ON"
@@ -88,7 +87,7 @@ enum PID{
 /**
  * RGB灯颜色
  */
-enum Color{
+enum Color {
     //%block="Red"
     RED = 1,
     //%block="Green"
@@ -102,7 +101,7 @@ enum Color{
     //%block="Cyan"
     CYAN = 6,
     //%block="White"
-    WHITH =7,
+    WHITH = 7,
     //%block="Put out"
     PUT = 8
 
@@ -114,7 +113,7 @@ namespace DFRobotMaqueenPluss {
      * 开启PID
      */
     //%block="PID switch|%pid"
-    export function PID(pid: PID):void {
+    export function PID(pid: PID): void {
         let buf = pins.createBuffer(2)
         buf[0] = 0x0A;
         buf[1] = pid;
@@ -127,28 +126,28 @@ namespace DFRobotMaqueenPluss {
     //% speed.min=0 speed.max=255
     export function MototRun(index: Motors, direction: Dir, speed: number): void {
         let buf = pins.createBuffer(5)
-        if(index == 1){
-            buf[0]=0x00;
-            buf[1]=direction;
-             buf[2]=speed;
-             buf[3]=0x03;
-             buf[4]=0;
-             pins.i2cWriteBuffer(0x10, buf)
+        if (index == 1) {
+            buf[0] = 0x00;
+            buf[1] = direction;
+            buf[2] = speed;
+            buf[3] = 0x03;
+            buf[4] = 0;
+            pins.i2cWriteBuffer(0x10, buf)
 
-        }else if(index == 2){
+        } else if (index == 2) {
             buf[0] = 0x02;
             buf[1] = direction;
             buf[2] = speed;
             buf[3] = 0x01;
             buf[4] = 0;
             pins.i2cWriteBuffer(0x10, buf)
-        }else if(index == 3){
+        } else if (index == 3) {
             buf[0] = 0x00;
             buf[1] = direction;
             buf[2] = speed;
             buf[3] = direction;
             buf[4] = speed;
-            pins.i2cWriteBuffer(0x10, buf)     
+            pins.i2cWriteBuffer(0x10, buf)
         }
 
     }
@@ -158,26 +157,26 @@ namespace DFRobotMaqueenPluss {
      */
     //% block="Motor Compensation|%mostors speed|%speed"
     //% speed.min=0 speed.max=255
-    export function MostotCompensation(mostor: Motors,speed:number): void {
-            let buf = pins.createBuffer(4)
-            if(mostor == 1){
-                buf[0] = 0x08;
-                buf[1] = speed;
-                buf[2] = 0x09;
-                buf[3] = 0;
-                pins.i2cWriteBuffer(0x10, buf)
-            }else if(mostor == 2){
-                buf[0] = 0x09;
-                buf[1] = speed;
-                buf[2] = 0x08;
-                buf[3] = 0
-                pins.i2cWriteBuffer(0x10, buf)
-            }else if(mostor == 3){
-                buf[0] = 0x08;
-                buf[1] = speed;
-                buf[2] = speed;
-                pins.i2cWriteBuffer(0x10, buf)
-            }
+    export function MostotCompensation(mostor: Motors, speed: number): void {
+        let buf = pins.createBuffer(4)
+        if (mostor == 1) {
+            buf[0] = 0x08;
+            buf[1] = speed;
+            buf[2] = 0x09;
+            buf[3] = 0;
+            pins.i2cWriteBuffer(0x10, buf)
+        } else if (mostor == 2) {
+            buf[0] = 0x09;
+            buf[1] = speed;
+            buf[2] = 0x08;
+            buf[3] = 0
+            pins.i2cWriteBuffer(0x10, buf)
+        } else if (mostor == 3) {
+            buf[0] = 0x08;
+            buf[1] = speed;
+            buf[2] = speed;
+            pins.i2cWriteBuffer(0x10, buf)
+        }
     }
     /**
      * 读电机转速
@@ -185,13 +184,13 @@ namespace DFRobotMaqueenPluss {
     //%block="read mostor|%index speed"
     export function ReadSpeed(index: Motors1): number {
         pins.i2cWriteNumber(0x10, 0, NumberFormat.Int8LE)
-        let x = pins.i2cReadBuffer(0x10, 4) 
+        let x = pins.i2cReadBuffer(0x10, 4)
         let y
-        if(index == 1){
-             y = x[1];
-             return y
-            
-        }else if(index == 2 ){
+        if (index == 1) {
+            y = x[1];
+            return y
+
+        } else if (index == 2) {
             y = x[3];
             return y
         }
@@ -205,15 +204,15 @@ namespace DFRobotMaqueenPluss {
     //% angle.min=0  angle.max=180
     export function ServoRun(index: Servos, angle: number): void {
         let buf = pins.createBuffer(2)
-        if(index == 1){
+        if (index == 1) {
             buf[0] = 0x14;
             buf[1] = angle;
             pins.i2cWriteBuffer(0x10, buf)
-        }else if (index == 2) {
+        } else if (index == 2) {
             buf[0] = 0x15;
             buf[1] = angle;
             pins.i2cWriteBuffer(0x10, buf)
-        }else if (index == 3) {
+        } else if (index == 3) {
             buf[0] = 0x16;
             buf[1] = angle;
             pins.i2cWriteBuffer(0x10, buf)
@@ -226,21 +225,21 @@ namespace DFRobotMaqueenPluss {
     //% block="|%rgbshow color|%color"
     export function SetRGBLight(rgb: RGBLight, color: Color): void {
         let buf = pins.createBuffer(3)
-        if(rgb == 1){
+        if (rgb == 1) {
             buf[0] = 0x0B
             buf[1] = color
             pins.i2cWriteBuffer(0x10, buf)
-        }else if(rgb == 2){
+        } else if (rgb == 2) {
             buf[0] = 0x0C
             buf[1] = color
             pins.i2cWriteBuffer(0x10, buf)
-        }else if(rgb == 3){
+        } else if (rgb == 3) {
             buf[0] = 0x0B
             buf[1] = color
             buf[2] = color
             pins.i2cWriteBuffer(0x10, buf)
         }
-        
+
     }
 
     /**
@@ -249,20 +248,19 @@ namespace DFRobotMaqueenPluss {
     //%block="read patrol|%patrol"
     export function ReadPatrol(patrol: Patrol): number {
         let x
-        if(patrol == 1)
-        {
+        if (patrol == 1) {
             pins.i2cWriteNumber(0x10, 0x1A, NumberFormat.Int8LE)
-             x =pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
-             return x
-        }else if (patrol == 2) {
+            x = pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
+            return x
+        } else if (patrol == 2) {
             pins.i2cWriteNumber(0x10, 0x19, NumberFormat.Int8LE)
             x = pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
             return x
-        }else if (patrol == 3) {
+        } else if (patrol == 3) {
             pins.i2cWriteNumber(0x10, 0x1B, NumberFormat.Int8LE)
             x = pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
             return x
-        }else if (patrol == 4) {
+        } else if (patrol == 4) {
             pins.i2cWriteNumber(0x10, 0x1C, NumberFormat.Int8LE)
             x = pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
             return x
@@ -273,18 +271,19 @@ namespace DFRobotMaqueenPluss {
      * 读版本号
      */
     //%block="get product information"
-    export function ReadVersion(): Buffer {
-        pins.i2cWriteNumber(0x10, 0x32, NumberFormat.Int8LE)
-        let v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE) 
-        pins.i2cWriteNumber(0x10, 0x33, NumberFormat.Int8LE)
-        let y = pins.i2cReadBuffer(0x10, v)
-        return y
+    export function ReadVersion(): number{
+        pins.i2cWriteNumber(0x10, 0x28, NumberFormat.Int8LE)
+        let v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE)
+        pins.i2cWriteNumber(0x10, 0x29, NumberFormat.Int8LE)
+        let  y = pins.i2cReadBuffer(0x10, v);
+        serial.writeBuffer(y)
+        return 0
     }
     /**
      * 超声波
      */
     //%block="ultrasonic T|%T E|%E |%sonic"
-    export function UltraSonic(T: DigitalPin, E: DigitalPin,sonic: Sonicunit):number {
+    export function UltraSonic(T: DigitalPin, E: DigitalPin, sonic: Sonicunit): number {
         let maxCmDistance = 500
         pins.setPull(T, PinPullMode.PullNone);
         pins.digitalWritePin(T, 0);
@@ -294,13 +293,13 @@ namespace DFRobotMaqueenPluss {
         pins.digitalWritePin(T, 0);
 
         pins.setPull(E, PinPullMode.PullUp);
-        let d = pins.pulseIn(E, PulseValue.High,maxCmDistance*42);
-        console.log("DISTANCE:"+d/42);
+        let d = pins.pulseIn(E, PulseValue.High, maxCmDistance * 42);
+        console.log("DISTANCE:" + d / 42);
         basic.pause(50);
-        let x = Math.round(d/42);
-        let y = Math.round(d/1);
+        let x = Math.round(d / 42);
+        let y = Math.round(d / 1);
 
-        switch(sonic){
+        switch (sonic) {
             case Sonicunit.Centimeters: return x
             default: return y
         }
