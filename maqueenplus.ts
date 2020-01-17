d191c6f637804a82e025c7708081f8418090e172
tangjie133 <1334875503@qq.com>
2020-01-17 14:52:29 +0800

    add

--- maqueenplus.ts
+++ maqueenplus.ts
@@ -1,4 +1,3 @@
-
 /** 
  * @file pxt-dfrobot_maqueenplus/maqueenplus.ts
  * @brief DFRobot's maqueen makecode library.
@@ -9,7 +8,7 @@
  * @copyright    MIT Lesser General Public License
  * 
  * @author [email](jie.tang@dfrobot.com)
- * @version  V0.0.6
+ * @version  V0.1.1
  * @date  2019-11-19
 */
 
@@ -25,9 +24,9 @@ enum PIN {
     P2 = 1,
     P8 = 18,
     //P9 = 10,
-    // P10 = 6,
+    P12 = 20,
     P13 = 23,
-    // P14 = 22,
+    P14 = 22,
     P15 = 21,
 };
 /**
@@ -92,12 +91,14 @@ enum Patrol {
     L1 = 1,
     //%block="L2"
     L2 = 2,
+    //%block="L3"
+    L3 = 5,
     //%block="R1"
     R1 = 3,
     //%block="R2"
     R2 = 4,
-    //%block="M"
-    M = 5
+    //%block="R3"
+    R3 = 6
 }
 
 /**
@@ -105,9 +106,7 @@ enum Patrol {
  */
 enum Sonicunit {
     //% block="cm"
-    Centimeters,
-    //% block="μm"
-    MicroSeconds
+    Centimeters
 }
 
 /**
@@ -131,7 +130,7 @@ enum Color {
     YELLOW = 3,
     //%block="Blue"
     BLUE = 4,
-    //%block="Pink"
+    //%block="Violet"
     PINK = 5,
     //%block="Cyan"
     CYAN = 6,
@@ -142,12 +141,44 @@ enum Color {
 
 }
 
-//% weight=100  color=#00A654   block="Maqueen Plus" icon="\uf136"
+//% weight=100  color=#00A654   block="Maqueen Plus" icon="\uf067"
 namespace DFRobotMaqueenPluss {
 
     export class Packeta {
         public mye: string;
         public myparam: number;
+    }
+   
+    /**
+    * I2C初始化
+    */
+    //%block="initialize via I2C until success"
+    export function I2CInit():void{
+        let Version_v = 0;
+        pins.i2cWriteNumber(0x10, 0x32, NumberFormat.Int8LE);
+        Version_v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE);
+        while (Version_v==0){
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `, 10)
+            basic.pause(500)
+            basic.clearScreen()
+            pins.i2cWriteNumber(0x10, 0x32, NumberFormat.Int8LE);
+            Version_v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE);
+        }
+        basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `, 10)
+        basic.pause(500)
+        basic.clearScreen()
     }
 
     /**
@@ -165,30 +196,29 @@ namespace DFRobotMaqueenPluss {
      */
     //% block="Motor|%index|direction|%direction|speed|%speed "
     //% speed.min=0 speed.max=255
-    export function MototRun(index: Motors, direction: Dir, speed: number): void {
-        let _speed:number;
-        _speed= Math.round(speed/1.11);
+    export function mototRun(index: Motors, direction: Dir, speed: number): void {
+
         if (index == 1) {
             let buf = pins.createBuffer(3)
             buf[0] = 0x00;
             buf[1] = direction;
-            buf[2] = _speed;
+            buf[2] = speed;
             pins.i2cWriteBuffer(0x10, buf)
 
         } if (index == 2) {
             let buf = pins.createBuffer(3)
             buf[0] = 0x02;
             buf[1] = direction;
-            buf[2] = _speed;
+            buf[2] = speed;
             pins.i2cWriteBuffer(0x10, buf)
         }
         if (index == 3) {
             let buf = pins.createBuffer(5)
             buf[0] = 0x00;
             buf[1] = direction;
-            buf[2] = _speed;
+            buf[2] = speed;
             buf[3] = direction;
-            buf[4] = _speed;
+            buf[4] = speed;
             pins.i2cWriteBuffer(0x10, buf)
         }
     }
@@ -196,7 +226,7 @@ namespace DFRobotMaqueenPluss {
     * 控制电机停止
     */
     //% block="Motor|%index stop"
-    export function MototStop(index: Motors): void {
+    export function mototStop(index: Motors): void {
 
         if (index == 1) {
             let buf = pins.createBuffer(3)
@@ -229,7 +259,7 @@ namespace DFRobotMaqueenPluss {
      */
     //% block="Motor Compensation|%motor speed|%speed"
     //% speed.min=0 speed.max=10
-    export function MostotCompensation(motor: Motors1, speed: number): void {
+    export function mostotCompensation(motor: Motors1, speed: number): void {
         let buf = pins.createBuffer(2)
         if (motor == 1) {
             buf[0] = 0x08;
@@ -245,14 +275,14 @@ namespace DFRobotMaqueenPluss {
      * 读电机转速
      */
     //%block="read Motor|%index speed"
-    export function ReadSpeed(index: Motors1): number {
-        let _speed:number;
+    export function readSpeed(index: Motors1): number {
         pins.i2cWriteNumber(0x10, 0, NumberFormat.Int8LE)
-        let x = pins.i2cReadBuffer(0x10, 4)
+        let speed_x = pins.i2cReadBuffer(0x10, 4)
         if (index == 1) {
-            return _speed=Math.round(x[1]*1.11);
+            return speed_x[1]
+
         } else if (index == 2) {
-            return _speed = Math.round(x[3] * 1.11);
+            return speed_x[3]
         }
         return -1
     }
@@ -260,14 +290,14 @@ namespace DFRobotMaqueenPluss {
      * 读电机正反转
      */
     //%block="read Motor|%index direction(stop:0,forward:1,back:2)"
-    export function ReadDirection(index: Motors1): number {
+    export function readDirection(index: Motors1): number {
         pins.i2cWriteNumber(0x10, 0, NumberFormat.Int8LE)
-        let x = pins.i2cReadBuffer(0x10, 4)
+        let dir_x = pins.i2cReadBuffer(0x10, 4)
         if (index == 1) {
-            return x[0]
+            return dir_x[0]
 
         } else if (index == 2) {
-            return x[2]
+            return dir_x[2]
         }
         return -1
     }
@@ -277,7 +307,7 @@ namespace DFRobotMaqueenPluss {
      */
     //% block="servo|%index|angle|%angle"
     //% angle.min=0  angle.max=180
-    export function ServoRun(index: Servos, angle: number): void {
+    export function servoRun(index: Servos, angle: number): void {
         let buf = pins.createBuffer(2)
         switch (index) {
             case 1:
@@ -302,8 +332,8 @@ namespace DFRobotMaqueenPluss {
      * RGB灯
      */
     //% block="set |%rgbshow color|%color"
-    export function SetRGBLight(rgbshow: RGBLight, color: Color): void {
-        
+    export function setRGBLight(rgbshow: RGBLight, color: Color): void {
+
         if (rgbshow == 1) {
             let buf = pins.createBuffer(2)
             buf[0] = 0x0B;
@@ -328,18 +358,19 @@ namespace DFRobotMaqueenPluss {
      * 寻线传感器
      */
     //%block="read patrol sensor|%patrol"
-    export function ReadPatrol(patrol: Patrol): number {
+    export function readPatrol(patrol: Patrol): number {
         pins.i2cWriteNumber(0x10, 0x1D, NumberFormat.Int8LE);
-        let _y = pins.i2cReadBuffer(0x10, 1);
-        let mark: number;
+        let patrol_y = pins.i2cReadBuffer(0x10, 1);
+        let mark: number ;
         switch (patrol) {
-            case 1: mark = (_y[0] & 0x02) == 0x02 ? 1 : 0; break;
-            case 2: mark = (_y[0] & 0x01) == 0x01 ? 1 : 0; break;
-            case 3: mark = (_y[0] & 0x04) == 0x04 ? 1 : 0; break;
-            case 4: mark = (_y[0] & 0x08) == 0x08 ? 1 : 0; break;
-            case 5: mark = (_y[0] & 0x10) == 0x10 ? 1 : 0; break;
+            case 1: mark = (patrol_y[0] & 0x04) == 0x04 ? 1 : 0; break;
+            case 2: mark = (patrol_y[0] & 0x02) == 0x02 ? 1 : 0; break;
+            case 3: mark = (patrol_y[0] & 0x08) == 0x08 ? 1 : 0; break;
+            case 4: mark = (patrol_y[0] & 0x10) == 0x10 ? 1 : 0; break;
+            case 5: mark = (patrol_y[0] & 0x01) == 0x01 ? 1 : 0; break;
+            case 6: mark = (patrol_y[0] & 0x20) == 0x20 ? 1 : 0; break;
         }
-
+        
         return mark
     }
 
@@ -347,25 +378,28 @@ namespace DFRobotMaqueenPluss {
      * 读取巡线传感器灰度值
      */
     //% block="read patrol sensor|%patrol voltage "
-    export function ReadPatrolVoltage(patrol: Patrol): number {
+    export function readPatrolVoltage(patrol: Patrol): number {
         pins.i2cWriteNumber(0x10, 0x1E, NumberFormat.Int8LE);
-        let y = pins.i2cReadBuffer(0x10, 10);
+        let patrolv_y = pins.i2cReadBuffer(0x10, 12);
         let patrol_AD: number;
         switch (patrol) {
             case 1:
-                patrol_AD = y[3] | y[2] << 8;
+                patrol_AD = patrolv_y[5] | patrolv_y[4] << 8;
                 break;
             case 2:
-                patrol_AD = y[1] | y[0] << 8;
+                patrol_AD = patrolv_y[3] | patrolv_y[2] << 8;
                 break;
             case 3:
-                patrol_AD = y[5] | y[4] << 8;
+                patrol_AD = patrolv_y[7] | patrolv_y[6] << 8;
                 break;
             case 4:
-                patrol_AD = y[7] | y[6] << 8;
+                patrol_AD = patrolv_y[9] | patrolv_y[8] << 8;
+                break;
+            case 5:
+                patrol_AD = patrolv_y[1] | patrolv_y[0] << 8;
                 break;
             default:
-                patrol_AD = y[9] | y[8] << 8;
+                patrol_AD = patrolv_y[11] | patrolv_y[10] << 8;
                 break;
 
         }
@@ -375,19 +409,19 @@ namespace DFRobotMaqueenPluss {
      * 读版本号
      */
     //%block="get product information"
-    export function ReadVersion(): string {
+    export function readVersion(): string {
         pins.i2cWriteNumber(0x10, 0x32, NumberFormat.Int8LE);
-        let _v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE);
+        let Version_v = pins.i2cReadNumber(0x10, NumberFormat.Int8LE);
         pins.i2cWriteNumber(0x10, 0x33, NumberFormat.Int8LE);
-        let _y = pins.i2cReadBuffer(0x10, _v);
-        let _x = _y.toString();
-        return _x;
+        let Version_y = pins.i2cReadBuffer(0x10, Version_v);
+        let Version_x = Version_y.toString();
+        return Version_x;
     }
     /**
      * 超声波
      */
-    //%block="read ultrasonic sensor T|%T E|%E |%sonic"
-    export function UltraSonic(T: PIN, E: PIN, sonic: Sonicunit): number {
+    //%block="read ultrasonic sensor T|%T E|%E cm"
+    export function ultraSonic(T: PIN, E: PIN): number {
         let maxCmDistance = 500;
         let _T;
         let _E;
@@ -396,10 +430,10 @@ namespace DFRobotMaqueenPluss {
             case PIN.P1: _T = DigitalPin.P1; break;
             case PIN.P2: _T = DigitalPin.P2; break;
             case PIN.P8: _T = DigitalPin.P8; break;
-            //case PIN.P9: _T = DigitalPin.P9; break;
+            case PIN.P12: _T = DigitalPin.P12; break;
             // case PIN.P10: _T = DigitalPin.P10; break;
             case PIN.P13: _T = DigitalPin.P13; break;
-            // case PIN.P14: _T = DigitalPin.P14; break;
+            case PIN.P14: _T = DigitalPin.P14; break;
             case PIN.P15: _T = DigitalPin.P15; break;
             default: _T = DigitalPin.P0; break;
         }
@@ -410,34 +444,35 @@ namespace DFRobotMaqueenPluss {
             case PIN.P2: _E = DigitalPin.P2; break;
             case PIN.P8: _E = DigitalPin.P8; break;
             //case PIN.P9: _E = DigitalPin.P9; break;
-            // case PIN.P10: _E = DigitalPin.P10; break;
+            case PIN.P12: _E = DigitalPin.P12; break;
             case PIN.P13: _E = DigitalPin.P13; break;
-            // case PIN.P14: _E = DigitalPin.P14; break;
+            case PIN.P14: _E = DigitalPin.P14; break;
             case PIN.P15: _E = DigitalPin.P15; break;
             default: _E = DigitalPin.P0; break;
         }
 
-        let d;
+        let ultraSonic_d;
         pins.digitalWritePin(_T, 0);
         if (pins.digitalReadPin(_E) == 0) {
             pins.digitalWritePin(_T, 1);
             // basic.pause(10);
             pins.digitalWritePin(_T, 0);
-            d = pins.pulseIn(_E, PulseValue.High, maxCmDistance * 58);
+            ultraSonic_d = pins.pulseIn(_E, PulseValue.High, maxCmDistance * 58);
         } else {
             pins.digitalWritePin(_T, 0);
             // basic.pause(10);
             pins.digitalWritePin(_T, 1);
-            d = pins.pulseIn(_E, PulseValue.Low, maxCmDistance * 58);
+            ultraSonic_d = pins.pulseIn(_E, PulseValue.Low, maxCmDistance * 58);
         }
-        let x = d / 39;
-        if (x <= 0 || x > 500) {
+        let ultraSonic_x = ultraSonic_d / 39;
+        if (ultraSonic_x <= 0 || ultraSonic_x > 500) {
             return 0;
         }
-        switch (sonic) {
-            case Sonicunit.Centimeters: return Math.round(x);
-            default: return Math.idiv(d, 2.54);
-        }
+        // switch (sonic) {
+        //     case Sonicunit.Centimeters: 
+        //     default: return Math.round(ultraSonic_x/2.54) //Math.idiv(ultraSonic_d, 2.54);
+        // }
+        return Math.round(ultraSonic_x);
 
 
     }
